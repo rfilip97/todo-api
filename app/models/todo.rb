9e707733caabed841202481d2a76f926f38ff760
Filip Razvan <filip.razvan@agilefreaks.com>
2023-06-12 12:36:11 +0300

    Update todo validation

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,14 +1,12 @@
 class Todo < ApplicationRecord
-  before_create :set_defaults
+  before_validation :set_defaults, on: :create
 
   validates :title, presence: true
 
   private
 
   def set_defaults
-    last_position = Todo.maximum(:position) || 0
-
-    self.position = last_position + 1
+    self.position = Todo.maximum(:position).to_i + 1
     self.completed = false
   end
 end
