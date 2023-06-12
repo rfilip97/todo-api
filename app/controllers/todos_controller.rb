8a0902604deee5466c49077f2b7d3b88b8e11693
Filip Razvan <filip.razvan@agilefreaks.com>
2023-06-12 14:20:52 +0300

    Add create action

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,6 @@
 class TodosController < ApplicationController
+  before_action :find_todo, only: [:update]
+
   def index
     @todos = Todo.all
     render json: @todos
@@ -13,9 +15,21 @@ class TodosController < ApplicationController
     end
   end
 
+  def update
+    if @todo.update(todo_params)
+      render json: @todo, status: :ok
+    else
+      render json: @todo.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def todo_params
     params.permit(:title, :position, :completed)
+  end
+
+  def find_todo
+    @todo = Todo.find(params[:id])
   end
 end
