b0a0adbf39a8b519f45114a717bab5a7edd36a9b
Filip Razvan <filip.razvan@agilefreaks.com>
2023-06-12 13:00:43 +0300

    Add specs for default values

--- spec/controllers/todos_controller_spec.rb
+++ spec/controllers/todos_controller_spec.rb
@@ -1,16 +1,18 @@
 require "rails_helper"
 
 RSpec.describe TodosController, type: :controller do
-  let!(:first_todo) { create(:todo) }
-  let!(:second_todo) { create(:todo) }
-  let!(:third_todo) { create(:todo) }
+  describe "GET #index" do
+    let!(:first_todo) { create(:todo) }
+    let!(:second_todo) { create(:todo) }
+    let!(:third_todo) { create(:todo) }
 
-  it "returns all todos" do
-    get :index
-    expect(response).to have_http_status(:ok)
+    it "returns all todos" do
+      get :index
+      expect(response).to have_http_status(:ok)
 
-    json_response = JSON.parse(response.body)
-    expect(json_response.length).to eq(3)
-    expect(json_response.map { |todo| todo["id"] }).to match_array([first_todo.id, second_todo.id, third_todo.id])
+      json_response = JSON.parse(response.body)
+      expect(json_response.length).to eq(3)
+      expect(json_response.map { |todo| todo["id"] }).to match_array([first_todo.id, second_todo.id, third_todo.id])
+    end
   end
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -15,5 +15,22 @@ RSpec.describe Todo, type: :model do
 
       it { expect(todo).not_to be_valid }
     end
+
+    context "default values" do
+      let!(:todo) { FactoryBot.create(:todo) }
+
+      it { expect(todo).to be_valid }
+      it { expect(todo.completed).to be false }
+      it { expect(todo.position).to be 1 }
+    end
+
+    context "increment default position" do
+      let!(:todo) { FactoryBot.create(:todo) }
+      let!(:another_todo) { FactoryBot.create(:todo) }
+
+      it { expect(todo).to be_valid }
+      it { expect(another_todo).to be_valid }
+      it { expect(another_todo.position).to be 2 }
+    end
   end
 end
