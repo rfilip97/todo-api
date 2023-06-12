760bb846aacc1c4167bb8f9ca8dabfd4c83b1ef0
Filip Razvan <filip.razvan@agilefreaks.com>
2023-06-12 12:19:38 +0300

    Add specs

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,19 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#validations" do
+    it { should validate_presence_of(:title) }
+
+    context "with valid params" do
+      todo = FactoryBot.build(:todo)
+
+      it { expect(todo).to be_valid }
+    end
+
+    context "with invalid title" do
+      todo = FactoryBot.build(:todo, title: nil)
+
+      it { expect(todo).not_to be_valid }
+    end
+  end
 end
