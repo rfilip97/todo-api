8957db9c546e0c0a05cce62ab579f179ef7abbb3
Filip Razvan <filip.razvan@agilefreaks.com>
2023-06-12 13:25:37 +0300

    Add create todo action

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,4 +3,19 @@ class TodosController < ApplicationController
     @todos = Todo.all
     render json: @todos
   end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      render json: @todo, status: :created
+    else
+      render json: @todo.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def todo_params
+    params.permit(:title, :position, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :todos, only: [:index], defaults: { format: :json }
+  resources :todos, only: [:index, :create], defaults: { format: :json }
 end
